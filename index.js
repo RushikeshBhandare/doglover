b29576caca20048c48c80e73ef15374ad56194d2
RushikeshBhandare <rushi5049@gmail.com>
2021-06-25 15:48:28 +0530

    change in express.static path

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json())
 
 if(process.env.NODE_ENV == "production"){
     //Set Static Folder
-    app.use(express.static('frontend/build'))
+    app.use(express.static('client/build'))
     app.get('*', (req,res)=>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
